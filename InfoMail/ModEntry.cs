0723dd84f616311db9dfa40f59a5cfb5a5f674d6
Markus Thulin <macky_b@hotmail.com>
2024-04-05 20:47:31 +0200

    Added InfoMail mod

---
+++ InfoMail/ModEntry.cs
@@ -0,0 +1,55 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.TerrainFeatures;
+using System.Linq;
+using System.Text;
+
+public class CropNotifierMod : Mod
+{
+    public override void Entry(IModHelper helper)
+    {
+        helper.Events.GameLoop.DayStarted += OnDayStarted;
+    }
+
+    private void OnDayStarted(object sender, DayStartedEventArgs e)
+    {
+        StringBuilder letter = new StringBuilder("Dear Farmer,\n\n");
+
+        bool hasCrops = false;
+        hasCrops |= CheckLocationForCrops(letter, Game1.getFarm(), "Farm");
+        hasCrops |= CheckLocationForCrops(letter, Game1.getLocationFromName("Greenhouse"), "Greenhouse");
+        hasCrops |= CheckLocationForCrops(letter, Game1.getLocationFromName("IslandWest"), "Ginger Island");
+
+        if (hasCrops)
+        {
+            letter.Append("\nYours,\nCrop Notifier");
+            Game1.mailbox.Add(letter.ToString());
+        }
+    }
+
+    private bool CheckLocationForCrops(StringBuilder letter, GameLocation location, string locationName)
+    {
+        if (location == null)
+            return false;
+
+        var cropsNeedWatering = location.terrainFeatures.Values.OfType<HoeDirt>()
+            .Where(hd => hd.crop != null && !hd.crop.dead && !hd.state.Value).Any();
+        var cropsReadyToHarvest = location.terrainFeatures.Values.OfType<HoeDirt>()
+            .Where(hd => hd.crop != null && hd.crop.currentPhase.Value >= hd.crop.phaseDays.Count - 1).Any();
+
+        if (cropsNeedWatering || cropsReadyToHarvest)
+        {
+            letter.Append($"At the {locationName}:\n");
+            if (cropsNeedWatering)
+                letter.Append("Some of your crops need watering.\n");
+            if (cropsReadyToHarvest)
+                letter.Append("Some of your crops are ready to harvest.\n");
+            letter.Append("\n");
+
+            return true;
+        }
+
+        return false;
+    }
+}
